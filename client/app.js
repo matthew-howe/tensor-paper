92eac336dfb8cc909b222bf43a1f8a8f38865c10
Matthew Howe <mattehowe@gmail.com>
2019-09-14 22:13:24 -0400

    Updates styling

--- client/app.js
+++ client/app.js
@@ -8,7 +8,7 @@ const App = () => {
         <div>
       <a
         className="github-link"
-        href="https://github.com/thetensorgroup/tensorpaperscissors"
+        href="https://github.com/matthew-howe/tensorpaperscissors"
         title="Source on GitHub"
       >
               <img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149" className="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"/>
