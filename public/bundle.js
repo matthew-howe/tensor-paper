e7d0ec9324341ac1812f7fc160a2b9395262a87c
matthew-howe <mattehowe@gmail.com>
2020-03-10 17:40:57 -0400

    responsive veiwport adjustments

--- public/bundle.js
+++ public/bundle.js
@@ -368,11 +368,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _material_ui_core_Button__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @material-ui/core/Button */ "./node_modules/@material-ui/core/esm/Button/index.js");
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -394,16 +394,7 @@ function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || func
 
 
 
- // import { makeStyles } from '@material-ui/core/styles';
 
- // const useStyles = makeStyles(theme => ({
-//   button: {
-//     margin: theme.spacing(1),
-//   },
-//   input: {
-//     display: 'none',
-//   },
-// }));
 
 var index = 0;
 var model = null;
@@ -443,57 +434,14 @@ function (_Component) {
 
   _createClass(Tensor, [{
     key: "componentWillMount",
-    value: function () {
-      var _componentWillMount = _asyncToGenerator(
-      /*#__PURE__*/
-      regeneratorRuntime.mark(function _callee() {
-        return regeneratorRuntime.wrap(function _callee$(_context) {
-          while (1) {
-            switch (_context.prev = _context.next) {
-              case 0:
-                _context.next = 2;
-                return this.props.fetchData();
-
-              case 2:
-              case "end":
-                return _context.stop();
-            }
-          }
-        }, _callee, this);
-      }));
-
-      function componentWillMount() {
-        return _componentWillMount.apply(this, arguments);
-      }
-
-      return componentWillMount;
-    }()
+    value: function componentWillMount() {
+      this.props.fetchData();
+    }
   }, {
     key: "componentDidMount",
-    value: function () {
-      var _componentDidMount = _asyncToGenerator(
-      /*#__PURE__*/
-      regeneratorRuntime.mark(function _callee2() {
-        return regeneratorRuntime.wrap(function _callee2$(_context2) {
-          while (1) {
-            switch (_context2.prev = _context2.next) {
-              case 0:
-                this.props.getRounds();
-
-              case 1:
-              case "end":
-                return _context2.stop();
-            }
-          }
-        }, _callee2, this);
-      }));
-
-      function componentDidMount() {
-        return _componentDidMount.apply(this, arguments);
-      }
-
-      return componentDidMount;
-    }()
+    value: function componentDidMount() {
+      this.props.getRounds();
+    }
   }, {
     key: "userButton",
     value: function userButton(img) {
@@ -703,12 +651,12 @@ function (_Component) {
       function () {
         var _ref = _asyncToGenerator(
         /*#__PURE__*/
-        regeneratorRuntime.mark(function _callee4() {
-          return regeneratorRuntime.wrap(function _callee4$(_context4) {
+        regeneratorRuntime.mark(function _callee2() {
+          return regeneratorRuntime.wrap(function _callee2$(_context2) {
             while (1) {
-              switch (_context4.prev = _context4.next) {
+              switch (_context2.prev = _context2.next) {
                 case 0:
-                  _context4.next = 2;
+                  _context2.next = 2;
                   return model.fit(xs, ys, {
                     shuffle: true,
                     validationSplit: 0.2,
@@ -743,20 +691,20 @@ function (_Component) {
                       onBatchEnd: function () {
                         var _onBatchEnd = _asyncToGenerator(
                         /*#__PURE__*/
-                        regeneratorRuntime.mark(function _callee3(batch, logs) {
-                          return regeneratorRuntime.wrap(function _callee3$(_context3) {
+                        regeneratorRuntime.mark(function _callee(batch, logs) {
+                          return regeneratorRuntime.wrap(function _callee$(_context) {
                             while (1) {
-                              switch (_context3.prev = _context3.next) {
+                              switch (_context.prev = _context.next) {
                                 case 0:
-                                  _context3.next = 2;
+                                  _context.next = 2;
                                   return tf.nextFrame();
 
                                 case 2:
                                 case "end":
-                                  return _context3.stop();
+                                  return _context.stop();
                               }
                             }
-                          }, _callee3);
+                          }, _callee);
                         }));
 
                         function onBatchEnd(_x, _x2) {
@@ -773,10 +721,10 @@ function (_Component) {
 
                 case 2:
                 case "end":
-                  return _context4.stop();
+                  return _context2.stop();
               }
             }
-          }, _callee4);
+          }, _callee2);
         }));
 
         return function train() {
